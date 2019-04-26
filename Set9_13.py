7135a6881667eca19aac6b3b455e0811bf25f66c
bmahesh123 <49895396+bmahesh123@users.noreply.github.com>
2019-04-26 17:55:24 +0530

    Delete Set9_13.py

--- Set9_13.py
+++
@@ -1,8 +0,0 @@
-
-class Set9:
-    # First Method
-    set_a = set(["Python", "Java", "Django", "Sql"])
-    set_b = set(["Phy", "sci", "Math", "History", "Python"])
-    print(set_a.symmetric_difference(set_b))
-
-    print(set_a^set_b)
