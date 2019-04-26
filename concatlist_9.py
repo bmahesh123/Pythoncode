599557134ebb66df56b3563a86bbae71bbeef53a
bmahesh123 <49895396+bmahesh123@users.noreply.github.com>
2019-04-26 17:54:20 +0530

    Delete concatlist_9.py

--- concatlist_9.py
+++
@@ -1,8 +0,0 @@
-def list_concat(list):
-    output= ''
-    for data in list:
-        output = output + str(data)
-        print(str(data))
-    return output
-
-print(list_concat([2, 4, 6, 8, 10]))
